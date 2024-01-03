07214d42236cf1b00add47b64b4fd4161ed7f985
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-01-03 12:06:08 +0300

    question 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,9 +15,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	h_left = binary_tree_height(tree->left);
 	h_right = binary_tree_height(tree->right);
-
-	if (h_left > h_right)
-		return (h_left + 1);
-	else
-		return (h_right +1);
+	
+	return (h_left > h_right) ? (h_left + 1) : (h_right + 1);
 }
