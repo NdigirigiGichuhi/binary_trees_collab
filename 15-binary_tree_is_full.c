a48df76566299b61e96f95f68bbf1a681b1bf29f
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-01-03 20:18:38 +0300

    question 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,9 +12,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (1);
 
+	/*
 	if ((tree->left == NULL && tree->right != NULL)
 			|| (tree->left != NULL && tree->right == NULL))
 		return (0);
+	*/
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
